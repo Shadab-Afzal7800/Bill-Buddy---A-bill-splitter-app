73b4701390f9e207ac62f433cf3e6a0e6f066704
ShadabAfzal <shadabafzal7800@gmail.com>
2023-03-19 18:17:31 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:bill_splitter/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: BillSplitter(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
